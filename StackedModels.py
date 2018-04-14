71ef25047e15609e67ef07dfbd26682dee86a18e
FranziOschi <oschmannfranziska@gmail.com>
2018-04-14 19:26:42 +0200

    Implementation of model simple stacking.

---
+++ StackedModels.py
@@ -0,0 +1,36 @@
+__author__ = 'franzi'
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+
+class StackingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+
+    def __init__(self, base_models, meta_model):
+
+        self.base_models = base_models
+        self.meta_model = meta_model
+
+    def fit(self, X, y):
+        self.base_models_ = [clone(x) for x in self.base_models]
+
+        for base_model in self.base_models_:
+            base_model.fit(X, y)
+
+        predictions = np.column_stack([
+            base_model.predict(X) for base_model in self.base_models_
+        ])
+
+        self.meta_model_ = clone(self.meta_model)
+
+        self.meta_model_.fit(predictions, y)
+
+        return self
+
+    def predict(self, X):
+
+        predictions = np.column_stack([
+            base_model.predict(X) for base_model in self.base_models_
+        ])
+
+        return self.meta_model_.predict(predictions)
+
